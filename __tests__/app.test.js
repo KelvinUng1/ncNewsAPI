9fa52c582f21cc79b9b0fbb1151bab23d367caae
Kelvin Ung <Kelvin@Kelvins-MacBook-Pro.local>
2024-01-15 15:36:21 +0000

    add Q3 solution

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,9 @@
 const request = require("supertest");
 const app = require("../app");
 const db = require("../db/connection");
-
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
+const endPoints =require("../endpoints.json")
 
 beforeEach(() => {
   return seed(testData);
@@ -14,12 +14,12 @@ afterAll(() => {
 });
 
 describe("/api/topics", () => {
-  test("GET:200 sends an array of topics", () => {
+  test("GET:200 responds with an array of topics", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
       .then((response) => {
-        expect(response.body.topics.length).toBe(3)
+        expect(response.body.topics.length).toBe(3);
         response.body.topics.forEach((topic) => {
           expect(topic).toHaveProperty("slug");
           expect(topic).toHaveProperty("description");
@@ -27,14 +27,24 @@ describe("/api/topics", () => {
       });
   });
 
-  test("GET:400 sends an appropriate message when given an invalid path", () => {
+  test("GET:400 responds with an appropriate message when given an invalid path", () => {
     return request(app)
       .get("/api/topicnotatopic")
       .expect(400)
-      .then((response) => { 
-          
-        expect(response.body.msg).toBe('Bad Request')
-       
-        });
+      .then((response) => {
+        expect(response.body.msg).toBe("Bad Request");
       });
   });
+});
+
+describe("/api", () => {
+  test("GET:200 provides a description of all other endpoints available", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+       
+        expect(response.body.endPoints).toEqual(endPoints)
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const endPoints = require("./endpoints.json")
 
-const{ getTopics } = require('./controllers/topics.controller')
+const { getTopics } = require("./controllers/topics.controller");
 
-app.get('/api/topics', getTopics);
+app.get("/api/topics", getTopics);
 
+app.get("/api", (req, res) => {
+    console.log(endPoints)
+    console.log({endPoints},"<<<<")
+    res.status(200).send({endPoints})
+})
 
-app.all('*', (req,res)=>{
-    console.log("caught error")
-    res.status(400).send({ msg: 'Bad Request'})
-    })
+app.all("*", (req, res) => {
+  res.status(400).send({ msg: "Bad Request" });
+});
 
 app.use((err, req, res, next) => {
-        console.log(err)
-    res.status(500).send({msg: "Internal Server Error"}) 
-})
-module.exports = app
+  console.log(err);
+  res.status(500).send({ msg: "Internal Server Error" });
+});
+module.exports = app;
