8722e5f36cd75c197e0b1e6b94efb31fde5f9b4d
Kelvin Ung <Kelvin@Kelvins-MacBook-Pro.local>
2024-01-16 10:36:17 +0000

    add Q4 solution get-api-articles/articleid

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const app = require("../app");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const testData = require("../db/data/test-data/index");
-const endPoints =require("../endpoints.json")
+const endPoints = require("../endpoints.json");
 
 beforeEach(() => {
   return seed(testData);
@@ -18,9 +18,9 @@ describe("/api/topics", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
-      .then((response) => {
-        expect(response.body.topics.length).toBe(3);
-        response.body.topics.forEach((topic) => {
+      .then(({ body }) => {
+        expect(body.topics.length).toBe(3);
+        body.topics.forEach((topic) => {
           expect(topic).toHaveProperty("slug");
           expect(topic).toHaveProperty("description");
         });
@@ -31,8 +31,8 @@ describe("/api/topics", () => {
     return request(app)
       .get("/api/topicnotatopic")
       .expect(400)
-      .then((response) => {
-        expect(response.body.msg).toBe("Bad Request");
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
       });
   });
 });
@@ -42,9 +42,47 @@ describe("/api", () => {
     return request(app)
       .get("/api")
       .expect(200)
-      .then((response) => {
-       
-        expect(response.body.endPoints).toEqual(endPoints)
+      .then(({ body }) => {
+        expect(body.endPoints).toEqual(endPoints);
+      });
+  });
+});
+
+describe("/api/articles/:article_id", () => {
+  test("GET:200 responds with an single article object with properties by id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
+        console.log(body, "<<<<");
+        expect(article).toBeInstanceOf(Object);
+        expect(article).toHaveProperty("author", expect.any(String));
+        expect(article).toHaveProperty("title", expect.any(String));
+        expect(article).toHaveProperty("article_id", expect.any(Number));
+        expect(article).toHaveProperty("body", expect.any(String));
+        expect(article).toHaveProperty("topic", expect.any(String));
+        expect(article).toHaveProperty("created_at", expect.any(String));
+        expect(article).toHaveProperty("votes", expect.any(Number));
+        expect(article).toHaveProperty("article_img_url", expect.any(String));
+      });
+  });
+
+  test("GET:404 responds with an appropriate status and message when given a valid but non-existent id", () => {
+    return request(app)
+      .get("/api/articles/999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Article does not exist");
+      });
+  });
+
+  test("GET:400 responds with an appropriate status and error message when given an invalid id", () => {
+    return request(app)
+      .get("/api/articles/not-an-article")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
       });
   });
 });
--- app.js
+++ app.js
@@ -2,19 +2,31 @@ const express = require("express");
 const app = express();
 const endPoints = require("./endpoints.json")
 
-const { getTopics } = require("./controllers/topics.controller");
+const { getTopics, getArticleById } = require("./controllers/topics.controller");
+
+
 
 app.get("/api/topics", getTopics);
 
 app.get("/api", (req, res) => {
-    console.log(endPoints)
-    console.log({endPoints},"<<<<")
     res.status(200).send({endPoints})
 })
 
+app.get("/api/articles/:article_id", getArticleById);
+
+
+
 app.all("*", (req, res) => {
-  res.status(400).send({ msg: "Bad Request" });
+  res.status(400).send({ msg: "Bad request" });
 });
+
+app.use((err,req,res,next) => {
+  if(err.code === '22P02'){
+    res.status(400).send({msg: "Bad request"})
+  } else if (err.status === 404){
+    res.status(404).send({msg: err.msg})
+  } else next(err)
+})
 
 app.use((err, req, res, next) => {
   console.log(err);
