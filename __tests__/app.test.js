721bdb37f023be945f0298c0d91af8b3cd1a2787
Kelvin Ung <Kelvin@Kelvins-MacBook-Pro.local>
2024-01-18 22:43:49 +0000

    add Q9-delete-comment-commentid

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,7 +13,7 @@ afterAll(() => {
   return db.end();
 });
 
-describe("/api/topics", () => {
+describe("GET /api/topics", () => {
   test("GET:200 responds with an array of topics", () => {
     return request(app)
       .get("/api/topics")
@@ -37,7 +37,7 @@ describe("/api/topics", () => {
   });
 });
 
-describe("/api", () => {
+describe("GET /api", () => {
   test("GET:200 provides a description of all other endpoints available", () => {
     return request(app)
       .get("/api")
@@ -48,7 +48,7 @@ describe("/api", () => {
   });
 });
 
-describe("/api/articles/:article_id", () => {
+describe("GET /api/articles/:article_id", () => {
   test("GET:200 responds with an single article object with properties by id", () => {
     return request(app)
       .get("/api/articles/1")
@@ -85,7 +85,7 @@ describe("/api/articles/:article_id", () => {
   });
 });
 
-describe("/api/articles", () => {
+describe("GET /api/articles", () => {
   test("GET:200 responds with an articles array of article objects", () => {
     return request(app)
       .get("/api/articles")
@@ -127,7 +127,7 @@ describe("/api/articles", () => {
   });
 });
 
-describe("/api/articles/:article_id/comments", () => {
+describe("GET /api/articles/:article_id/comments", () => {
   test("GET:200 responds with an array of comments belonging to a single article", () => {
     return request(app)
       .get("/api/articles/9/comments")
@@ -175,7 +175,7 @@ describe("/api/articles/:article_id/comments", () => {
   });
 });
 
-describe("/api/articles/:article_id/comments", () => {
+describe("POST /api/articles/:article_id/comments", () => {
   test("POST:201 responds with the posted comment with expected properties", () => {
     return request(app)
       .post("/api/articles/9/comments")
@@ -203,10 +203,10 @@ describe("/api/articles/:article_id/comments", () => {
       })
       .expect(404)
       .then(({ body }) => {
-        expect(body.msg).toBe("Article not found");
+        expect(body.msg).toBe("Article does not exist");
       });
   });
-  test("POST:400 responds with an appropriate status and error message for invalid id", () => {
+  test("POST:400 responds with an appropriate status and error message for invalid article id", () => {
     return request(app)
       .post("/api/articles/not-an-article/comments")
       .send({
@@ -228,7 +228,7 @@ describe("/api/articles/:article_id/comments", () => {
       })
       .expect(404)
       .then(({ body }) => {
-        expect(body.msg).toBe("User not found");
+        expect(body.msg).toBe("User does not exist");
       });
   });
 
@@ -237,7 +237,6 @@ describe("/api/articles/:article_id/comments", () => {
       .post("/api/articles/not-an-article/comments")
       .send({
         username: "butter_bridge",
-
       })
       .expect(400)
       .then(({ body }) => {
@@ -246,40 +245,75 @@ describe("/api/articles/:article_id/comments", () => {
   });
 });
 
-test("PATCH:200 responds with the updated article and updates the article votes by -100", () => {
-  return request(app)
-    .patch("/api/articles/1")
-    .send({ inc_votes: -100 })
-    .expect(200)
-    .then(({ body }) => {
-      const { article } = body;
-      expect(article).toHaveProperty("article_id", 1);
-      expect(article).toHaveProperty("title", expect.any(String));
-      expect(article).toHaveProperty("author", expect.any(String));
-      expect(article).toHaveProperty("body", expect.any(String));
-      expect(article).toHaveProperty("topic", expect.any(String));
-      expect(article).toHaveProperty("created_at", expect.any(String));
-      expect(article).toHaveProperty("votes", 0);
-      expect(article).toHaveProperty("article_img_url", expect.any(String));
-    });
-});
+describe("PATCH /api/articles/:article_id", () => {
+  test("PATCH:200 responds with the updated article and updates the article votes by -100", () => {
+    return request(app)
+      .patch("/api/articles/1")
+      .send({ inc_votes: -100 })
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
+        expect(article).toHaveProperty("article_id", 1);
+        expect(article).toHaveProperty("title", expect.any(String));
+        expect(article).toHaveProperty("author", expect.any(String));
+        expect(article).toHaveProperty("body", expect.any(String));
+        expect(article).toHaveProperty("topic", expect.any(String));
+        expect(article).toHaveProperty("created_at", expect.any(String));
+        expect(article).toHaveProperty("votes", 0);
+        expect(article).toHaveProperty("article_img_url", expect.any(String));
+      });
+  });
 
-test("PATCH:400 responds with an appropriate status and error message for invalid inc_votes", () => {
-  return request(app)
-    .patch("/api/articles/1")
-    .send({ inc_votes: "anystring" })
-    .expect(400)
-    .then(({ body }) => {
-      expect(body.msg).toBe("Bad request");
-    });
-});
+  test("PATCH:400 responds with an appropriate status and error message for invalid inc_votes", () => {
+    return request(app)
+      .patch("/api/articles/1")
+      .send({ inc_votes: "anystring" })
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
+      });
+  });
 
-test("PATCH:404 responds with an appropriate status and error message when given a valid but non-existent id", () => {
-  return request(app)
-    .patch("/api/articles/999")
-    .send({ inc_votes: 1 })
-    .expect(404)
-    .then(({ body }) => {
-      expect(body.msg).toBe("Article does not exist");
-    });
+  test("PATCH:404 responds with an appropriate status and error message when given a valid but non-existent id", () => {
+    return request(app)
+      .patch("/api/articles/999")
+      .send({ inc_votes: 1 })
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Article does not exist");
+      });
+  });
+
+  test("PATCH:400 responds with an appropriate status and error message for invalid id", () => {
+    return request(app)
+      .patch("/api/articles/not-an-id")
+      .send({ inc_votes: 1 })
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
+      });
+  });
 });
+describe("DELETE /api/comments/:comment_id", () => {
+  test("DELETE:204 deletes the specified comment by comment id and sends no body back ", () => {
+    return request(app).delete("/api/comments/1").expect(204);
+  });
+
+  test("DELETE:404 responds with an appropriate status and error message when given a non-existent id", () => {
+    return request(app)
+      .delete("/api/comments/999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Comment does not exist");
+      });
+  });
+
+  test("DELETE:400 responds with an appropriate status and error message when given an invalid id", () => {
+    return request(app)
+      .delete("/api/comments/not-an-id")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
+      });
+  });
+})
--- models/models.js
+++ models/models.js
@@ -79,37 +79,88 @@ module.exports.selectCommentsByArticleId = (article_id) => {
 };
 
 module.exports.insertComment = (article_id, username, body) => {
-
   return db
-  .query(
-    `INSERT INTO comments (article_id, author, body)
+    .query(
+      `SELECT * FROM articles
+  WHERE article_id =$1;
+  `,
+      [article_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Article does not exist",
+        });
+      }
+
+      return db
+        .query(
+          `SELECT * FROM users
+    WHERE username =$1;
+    `,
+          [username]
+        )
+        .then(({ rows }) => {
+          if (rows.length === 0) {
+            return Promise.reject({
+              status: 404,
+              msg: "User does not exist",
+            });
+          }
+          return db.query(
+            `INSERT INTO comments (article_id, author, body)
     VALUES($1, $2, $3)
     RETURNING *;
     `,
-        [article_id, username, body]
-  )
-  .then(({ rows }) => {
-    return rows[0]
-  })
-}
+            [article_id, username, body]
+          );
+        });
+    })
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
 
 module.exports.updateArticleVotes = (article_id, inc_votes) => {
   return db
-  .query(
-    `
+    .query(
+      `
     UPDATE articles
     SET votes = votes + $1
     WHERE article_id = $2
     RETURNING*;
-    `, [inc_votes, article_id]
-  )
-  .then(({rows})=> {
-    if(rows.length ===0){
-      return Promise.reject({
-        status: 404,
-        msg: "Article does not exist"
-      })
-    }
-    return rows[0]
-  })
-}
+    `,
+      [inc_votes, article_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Article does not exist",
+        });
+      }
+      return rows[0];
+    });
+};
+
+module.exports.removeCommentById = (comment_id) => {
+  return db
+    .query(
+      `
+  DELETE FROM comments
+  WHERE comment_id = $1
+  RETURNING *;
+  `,
+      [comment_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        throw {
+          status: 404,
+          msg: "Comment does not exist",
+        };
+      }
+      return rows[0];
+    });
+};
