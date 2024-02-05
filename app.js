cdccaf0fa8ab290ba69b4ab4b7d7c1143cba0338
Kelvin Ung <Kelvin@Kelvins-MacBook-Pro.local>
2024-02-05 10:04:12 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require("express");
 const app = express();
 
@@ -12,6 +13,8 @@ const {
   deleteCommentById,
   getUsers,
 } = require("./controllers/controller");
+
+app.use(cors());
 
 app.use(express.json());
 
